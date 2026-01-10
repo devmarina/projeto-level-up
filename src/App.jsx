c36343b4383843153fc3de472cbec0f74f33903a
devmarina <150974701+devmarina@users.noreply.github.com>
2026-01-10 12:09:54 -0300

    Estrutura de pages e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+// src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
+// Importando as páginas que criamos
+import Dashboard from "./pages/Dashboard";
+import Conteudos from "./pages/Conteudos";
+import Flashcards from "./pages/Flashcards";
+import Revisoes from "./pages/Revisoes";
+import Questionarios from "./pages/Questionarios";
+import Login from "./pages/Login";
 
 function App() {
-
   return (
-    <>
     <BrowserRouter>
-      {/* nav */}
-        <Routes>
-          {/* rotas */}
-          {/* <Route path="/about" element={<About />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+      <Header /> 
+
+      <Routes>
+        {/* Rota inicial */}
+        <Route path="/" element={<Login />} />
+        
+        {/* Rotas principais */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/conteudos" element={<Conteudos />} />
+        <Route path="/flashcards" element={<Flashcards />} />
+        <Route path="/revisoes" element={<Revisoes />} />
+        <Route path="/questionarios" element={<Questionarios />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Questionarios.jsx
@@ -0,0 +1,7 @@
+export default function Questionarios() {
+  return (
+    <div>
+      <h1>Tela Questionários</h1>
+    </div>
+  );
+}
