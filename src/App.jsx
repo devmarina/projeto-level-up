a8685f505287121a2f9e6cc0a33266524867f4b8
devmarina <150974701+devmarina@users.noreply.github.com>
2026-01-11 15:54:39 -0300

    Refactor routes in App component for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,27 @@ function AppContent() {
 
   return (
     <>
-      {!hideHeader && <Header />}
+      {" "}
+      {!hideHeader && <Header />}{" "}
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/conteudos" element={<Conteudos />} />
-        <Route path="/conteudos/detalhes" element={<ConteudoDisciplinaEspecifica />} />
-        <Route path="/flashcards" element={<Flashcards />} />
-        <Route path="/revisoes" element={<Revisoes />} />
-        <Route path="/questionarios" element={<Questionarios />} />
-        <Route path="/questionarios/detalhes" element={<QuestionariosDisciplinaEspecifico />} />
-      </Routes>
+        {" "}
+        <Route path="/" element={<Login />} />{" "}
+        <Route path="/cadastro" element={<Cadastro />} />{" "}
+        <Route path="/dashboard" element={<Dashboard />} />{" "}
+        <Route path="/conteudos" element={<Conteudos />} />{" "}
+        <Route
+          path="/conteudos/detalhes"
+          element={<ConteudoDisciplinaEspecifica />}
+        />{" "}
+        <Route path="/flashcards" element={<Flashcards />} />{" "}
+        <Route path="/revisoes" element={<Revisoes />} />{" "}
+        <Route path="/questionarios" element={<Questionarios />} />{" "}
+        <Route path="/questionario-ativo" element={<QuestionarioAtivo />} />
+        <Route
+          path="/questionarios/detalhes"
+          element={<QuestionariosDisciplinaEspecifico />}
+        />{" "}
+      </Routes>{" "}
     </>
   );
 }
